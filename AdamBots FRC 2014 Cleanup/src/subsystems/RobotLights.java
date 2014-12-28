97728a12d454e86e059f4e14740f828edb5027a8
Nathan Fenner <nfenneremail@gmail.com>
2014-12-28 14:57:56 -0500

    cleared pointless methods in RobotLights

--- AdamBots FRC 2014 Cleanup/src/subsystems/RobotLights.java
+++ AdamBots FRC 2014 Cleanup/src/subsystems/RobotLights.java
@@ -5,7 +5,6 @@
  */
 package subsystems;
 
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.templates.RobotActuators;
 
 /**
@@ -15,136 +14,19 @@ import edu.wpi.first.wpilibj.templates.RobotActuators;
 public abstract class RobotLights {
 
 ////VARIABLES-------------------------------------------------------------------
-    private DigitalInput liar;
-
 ////CONSTANTS-------------------------------------------------------------------
-    public void init() {
-        //INSERT CODE HERE
+	public static void underglowOn() {
+		RobotActuators.groundLEDStrip1.set(true);
+		RobotActuators.groundLEDStrip2.set(true);
+		RobotActuators.groundLEDStrip3.set(true);
+		RobotActuators.groundLEDStrip4.set(true);
 
-    }
+	}
 
-    //randomly turning on and off lights
-    /*public void swagMode() {
-        int strand = (int) MathUtils.rand(6) + 1;
-        switch (strand) {
-            case 1:
-                if (RobotActuators.groundLEDStrip1.get() == Relay.Value.kOn) {
-                    RobotActuators.groundLEDStrip1.set(Relay.Value.kOff);
-                } else {
-                    RobotActuators.groundLEDStrip1.set(Relay.Value.kOn);
-                }
-                break;
-            case 2:
-                if (RobotActuators.groundLEDStrip2.get() == Relay.Value.kOn) {
-                    RobotActuators.groundLEDStrip2.set(Relay.Value.kOff);
-                } else {
-                    RobotActuators.groundLEDStrip2.set(Relay.Value.kOn);
-                }
-
-                break;
-            case 3:
-                if (RobotActuators.groundLEDStrip3.get() == Relay.Value.kOn) {
-                    RobotActuators.groundLEDStrip3.set(Relay.Value.kOff);
-                } else {
-                    RobotActuators.groundLEDStrip3.set(Relay.Value.kOn);
-                }
-                break;
-            case 4:
-                if (RobotActuators.groundLEDStrip4.get() == Relay.Value.kOn) {
-                    RobotActuators.groundLEDStrip3.set(Relay.Value.kOff);
-                } else {
-                    RobotActuators.groundLEDStrip4.set(Relay.Value.kOn);
-                }
-
-                break;
-            case 5:
-                if (RobotActuators.LEDStripLeftPanel.get() == Relay.Value.kOn) {
-                    RobotActuators.LEDStripLeftPanel.set(Relay.Value.kOff);
-                } else {
-                    RobotActuators.LEDStripLeftPanel.set(Relay.Value.kOn);
-                }
-                break;
-            case 6:
-                if (RobotActuators.LEDStripRightPanel.get() == Relay.Value.kOn) {
-                    RobotActuators.LEDStripRightPanel.set(Relay.Value.kOff);
-                } else {
-                    RobotActuators.LEDStripRightPanel.set(Relay.Value.kOn);
-                }
-                break;
-            default:
-                System.out.println("rand: " + strand);
-                break;
-        }
-    }*/
-
-    public void shootingLights() {
-        //INSERT CODE HERE
-    }
-
-    public boolean ifReady() {
-        //INSERT CODE HERE
-        return true;
-    }
-
-    public boolean ifJammed() {
-        //INSERT CODE HERE
-        return true;
-    }
-
-    public void lightResponse() {
-        boolean ifReady = ifReady();
-        boolean ifJammed = ifJammed();
-        if (ifReady)
-	    ; else
-	    ; //Error message
-        if (ifJammed)
-	    ; //Error message
-        else
-	    ;
-
-        //INSERT CODE HERE
-    }
-
-    public static void underglowOn() {
-        //INSERT CODE HERE
-        // turns on
-        // RobotActuators.groundLEDStrip1.set(Relay.Value.kOn);
-        // turns off
-        // RobotActuators.groundLEDStrip1.set(Relay.Value.kOff);
-        RobotActuators.groundLEDStrip1.set(true);
-        RobotActuators.groundLEDStrip2.set(true);
-        RobotActuators.groundLEDStrip3.set(true);
-        RobotActuators.groundLEDStrip4.set(true);
-
-    }
-
-    public static void underglowOff() {
-        RobotActuators.groundLEDStrip1.set(false);
-        RobotActuators.groundLEDStrip2.set(false);
-        RobotActuators.groundLEDStrip3.set(false);
-        RobotActuators.groundLEDStrip4.set(false);
-    }
-
-    public void cameraSpotlight() {
-        //INSERT CODE HERE
-    }
-
-    public void confuse() {
-        //INSERT CODE HERE
-        for (int loop = 0; loop < 100; loop++) {
-            //turn on
-            //turn off
-            delay(50);
-        }
-    }
-
-    public void delay(int time) {
-        try {
-            Thread.sleep(time);
-        } catch (InterruptedException e) {
-            System.out.println(e);
-
-        }
-    }
-
+	public static void underglowOff() {
+		RobotActuators.groundLEDStrip1.set(false);
+		RobotActuators.groundLEDStrip2.set(false);
+		RobotActuators.groundLEDStrip3.set(false);
+		RobotActuators.groundLEDStrip4.set(false);
+	}
 }
