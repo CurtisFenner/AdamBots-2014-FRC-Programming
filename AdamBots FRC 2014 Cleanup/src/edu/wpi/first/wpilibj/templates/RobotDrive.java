ed85ac02d12610974c3654b62a26c597f73ff7c5
CurtisFenner <cfenneremail@gmail.com>
2014-12-28 12:54:38 -0500

    Renamed Project (Sorry)

---
+++ AdamBots FRC 2014 Cleanup/src/edu/wpi/first/wpilibj/templates/RobotDrive.java
@@ -0,0 +1,211 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates;
+
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ *
+ * @author Nathan Fenner
+ */
+public abstract class RobotDrive {
+
+	public static final double distancePerTick = 83.0 / 1109.0;
+	private static int encoderLastLeft = 0;
+	private static int encoderLastRight = 0;
+	private static double velocityLeft = 0;
+	private static double velocityRight = 0;
+	private static double targetSpeedLeft = 0.0;
+	private static double targetSpeedRight = 0.0;
+	private static double currentSpeedLeft = 0.0;
+	private static double currentSpeedRight = 0.0;
+	private static boolean smoothingEnabled = true;
+	private static Timer clock;
+
+////INIT------------------------------------------------------------------------
+	public static void initialize() {
+		clock = new Timer();
+		clock.start();
+		System.out.println("RobotDrive init");
+		RobotSensors.rightDriveEncoder.start();
+		RobotSensors.leftDriveEncoder.start();
+		RobotSensors.rightDriveEncoder.setDistancePerPulse((Math.PI * 0.5) / 360);
+		RobotSensors.leftDriveEncoder.setDistancePerPulse((Math.PI * 0.5) / 360);
+	}
+////METHODS---------------------------------------------------------------------
+	private static double shift_up = 3.0 / 10.0;
+	private static double shift_down = 20.0 / 100.0;
+
+	//4.283 smooth
+	//6.540 shift
+	/**
+	 * In inches
+	 *
+	 * @return
+	 */
+	public static double getEncoderLeftInches() {
+		return RobotSensors.leftDriveEncoder.get() * distancePerTick;
+	}
+
+	public static int getEncoderLeftTicks() {
+		return -RobotSensors.leftDriveEncoder.get();
+	}
+
+	/**
+	 * In inches
+	 *
+	 * @return
+	 */
+	public static double getEncoderRightInches() {
+		return RobotSensors.rightDriveEncoder.get() * distancePerTick;
+	}
+
+	public static int getEncoderRightTicks() {
+		return RobotSensors.rightDriveEncoder.get();
+	}
+
+	public static void update() {
+
+		double shift_left = (MathUtils.sign(targetSpeedLeft) == MathUtils.sign(targetSpeedLeft - currentSpeedLeft)) ? shift_up : shift_down;
+		double shift_right = (MathUtils.sign(targetSpeedRight) == MathUtils.sign(targetSpeedRight - currentSpeedRight)) ? shift_up : shift_down;
+
+		currentSpeedLeft += MathUtils.sign(targetSpeedLeft - currentSpeedLeft)
+				* Math.min(Math.abs(targetSpeedLeft - currentSpeedLeft), shift_left);
+		currentSpeedRight += MathUtils.sign(targetSpeedRight - currentSpeedRight)
+				* Math.min(Math.abs(targetSpeedRight - currentSpeedRight), shift_right);
+
+		double dt = clock.get();
+		clock.reset();
+
+		int leftEncoder = RobotSensors.leftDriveEncoder.get();
+		int rightEncoder = -RobotSensors.rightDriveEncoder.get();
+
+		velocityLeft = (leftEncoder - encoderLastLeft) / dt;
+		velocityRight = (rightEncoder - encoderLastRight) / dt;
+
+		encoderLastLeft = leftEncoder;
+		encoderLastRight = rightEncoder;
+
+		//SmartDashboard.putNumber("Current Left", currentSpeedLeft + RobotTeleop.DEBUG_OSCILLATE / 800.0);
+		//SmartDashboard.putNumber("Measured Left", pwmFromTPS(velocityLeft) + RobotTeleop.DEBUG_OSCILLATE / 800.0);
+		//SmartDashboard.putNumber("Target Left", targetSpeedLeft + RobotTeleop.DEBUG_OSCILLATE / 800.0);
+
+		//SmartDashboard.putNumber("Current Right", currentSpeedRight + RobotTeleop.DEBUG_OSCILLATE / 800.0);
+		//SmartDashboard.putNumber("Measured Right", pwmFromTPS(velocityRight) + RobotTeleop.DEBUG_OSCILLATE / 800.0);
+		//SmartDashboard.putNumber("Target Right", targetSpeedRight + RobotTeleop.DEBUG_OSCILLATE / 800.0);
+		//SmartDashboard.putNumber("Drive Encoder Left", getEncoderLeftTicks());
+		//SmartDashboard.putNumber("Drive Encoder Right", getEncoderRightTicks());
+
+		//SmartDashboard.putNumber("Shift Up", shift_up * 10000);
+		//SmartDashboard.putNumber("Shift Down", shift_down * 10000);
+
+
+		// Use currentSpeed and velocity to set raw
+		if (smoothingEnabled) {
+			RobotDrive.driveSetRaw(currentSpeedLeft, currentSpeedRight);
+		} else {
+			RobotDrive.driveSetRaw(targetSpeedLeft, targetSpeedRight);
+		}
+	}
+
+	public static double pwmFromRPM(double rpm) {
+		return pwmFromTPS(rpm / 60 * 360);
+	}
+
+	/**
+	 * Transforms a rotation rate to a PWM value
+	 *
+	 * @param tps Ticks per second
+	 * @return
+	 */
+	public static double pwmFromTPS(double tps) {
+		return (0.1139 * MathUtils.exp(0.0024 * Math.abs(tps)) - .1139) * MathUtils.sign(tps) / (0.987642579 - .1139);
+	}
+
+	/**
+	 * Sets drive speed to go forward
+	 *
+	 * @param speed
+	 */
+	public static void driveStraight(double speed) {
+		drive(speed, speed);
+	}
+
+	/**
+	 * Sets the left and right drive safely, which it fits into the [-1,1] range.
+	 *
+	 * @param leftSpeed
+	 * @param rightSpeed
+	 */
+	public static void drive(double leftSpeed, double rightSpeed) {
+		leftSpeed = Math.max(-1, Math.min(1, leftSpeed));
+		rightSpeed = Math.max(-1, Math.min(1, rightSpeed));
+
+		targetSpeedLeft = leftSpeed;
+		targetSpeedRight = rightSpeed;
+	}
+
+	/**
+	 * Raw setting speed, not smooth: avoid use whenever possible
+	 *
+	 * @param left
+	 * @param right
+	 */
+	private static void driveSetRaw(double leftSpeed, double rightSpeed) {
+		leftSpeed = Math.max(-1, Math.min(1, leftSpeed));
+		rightSpeed = Math.max(-1, Math.min(1, rightSpeed));
+		RobotActuators.leftDrive.set(leftSpeed);
+		RobotActuators.rightDrive.set(-rightSpeed);
+	}
+
+	/**
+	 * Sets the robot to turn in an arc
+	 *
+	 * @param turnRate Positive values turn right (clockwise)
+	 * @param forwardSpeed Positive values go forward
+	 */
+	public static void turn(double turnRate, double forwardSpeed) {
+		drive(forwardSpeed + turnRate, forwardSpeed - turnRate);
+	}
+
+	public static void shiftHigh() {
+		RobotActuators.shifter.set(true);
+	}
+
+	public static void shiftLow() {
+		RobotActuators.shifter.set(false);
+	}
+
+	// autoshifts gears
+	public static void autoShift(double sensitvity, boolean shiftLow /* in G's*/) {
+		double xAcceleration = RobotSensors.accelerometer.getAcceleration(ADXL345_I2C.Axes.kX);
+		double zAcceleration = RobotSensors.accelerometer.getAcceleration(ADXL345_I2C.Axes.kZ);
+		if (xAcceleration > sensitvity && zAcceleration > sensitvity) {
+			RobotActuators.shifter.set(false);
+		} else if (shiftLow == true || xAcceleration < sensitvity && zAcceleration
+				< sensitvity) {
+			RobotActuators.shifter.set(true);
+		} else {
+			RobotActuators.shifter.set(false);
+		}
+	}
+
+	public static void stopDrive() {
+		targetSpeedLeft = 0.0;
+		currentSpeedLeft = 0.0;
+		targetSpeedRight = 0.0;
+		currentSpeedRight = 0.0;
+	}
+
+	public static void disableSmoothing() {
+		smoothingEnabled = false;
+	}
+
+	public static void enableSmoothing() {
+		smoothingEnabled = true;
+	}
+}
---
+++ AdamBots FRC 2014 Cleanup/src/edu/wpi/first/wpilibj/templates/RobotPickup.java
@@ -0,0 +1,176 @@
+package edu.wpi.first.wpilibj.templates;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ *
+ * @author Nathan
+ */
+public class RobotPickup {
+
+	private static final double ANGLE_TOLERANCE = 10;                            //// TODO: CHANGE BACK TO 3
+	private static final double PICKUP_POSITION = -18;
+	//private static final double SHOOT_POSITION = 45.0;						// Practice robot
+	//private static final double SHOOT_POSITION = 48.0;						// Practice robot // Changed to bring angle up a few degrees.  Actually targeting 45
+	//private static final double TRUSS_POSITION = 55.0;
+	private static final double SHOOT_POSITION = 37.0;    //shoot position now 36 as of wednesday before kettering	
+        // competition robot // targeting 5 degrees less than the practice one
+	private static final double TRUSS_POSITION = 50.0;							// competition robot // targeting 5 degrees less than the practice one
+	private static final double CATCH_POSITION = 90;
+	private static double armTargetAngle = CATCH_POSITION;
+	private static double lastPosition = 0.0;
+	private static double velocity = 0.0;
+	private static Timer timer;
+	private static double lastTime = 0;
+
+	public static double getArmTargetAngle() {
+		return armTargetAngle;
+	}
+
+	public static void openRollerArm() {
+		if (getArmAngleAboveHorizontal() > 80 || isUpperLimitReached()) {
+			return;
+		}
+		RobotActuators.rollerArmUp.set(false);
+		RobotActuators.rollerArmDown.set(true);
+	}
+
+	public static void closeRollerArm() {
+		RobotActuators.rollerArmUp.set(true);
+		RobotActuators.rollerArmDown.set(false);
+	}
+
+	public static void neutralRollerArm() {
+		RobotActuators.rollerArmUp.set(false);
+		RobotActuators.rollerArmDown.set(false);
+	}
+
+	public static void setRollerSpeed(double speed) {
+		RobotActuators.pickupRollerArmMotor.set(speed);
+	}
+
+	public static void adjustArmAngle(double adjustAngle) {
+		armTargetAngle += adjustAngle;
+	}
+
+	public static boolean isUpperLimitReached() {
+		return RobotSensors.pickupSystemUpLim.get();
+	}
+
+	public static boolean isLowerLimitReached() {
+		return RobotSensors.pickupSystemDownLim.get();
+	}
+
+	public static boolean isBallInPickup() {
+		return RobotSensors.ballReadyToLiftLim.get();
+	}
+
+	public static void movePickupToAngle(double givenAngle) {
+		armTargetAngle = givenAngle;
+	}
+
+	public static void moveToPickupPosition() {
+		movePickupToAngle(PICKUP_POSITION);
+	}
+
+	public static void moveToShootPosition() {
+		movePickupToAngle(SHOOT_POSITION);
+	}
+
+	public static void moveToTrussPosition() {
+		movePickupToAngle(TRUSS_POSITION);
+	}
+
+	public static void moveToCatchPosition() {
+		movePickupToAngle(CATCH_POSITION);
+	}
+
+	public static boolean isPickupInPosition() {
+		return Math.abs(getArmAngleAboveHorizontal() - armTargetAngle) < ANGLE_TOLERANCE;
+	}
+
+	public static boolean isPickupInPosition(double angle) {
+		return Math.abs(getArmAngleAboveHorizontal() - angle) < ANGLE_TOLERANCE && armTargetAngle == angle && Math.abs(getVelocity()) < 50;
+	}
+
+	public static boolean isPickupInPickupPosition() {
+		return isPickupInPosition(PICKUP_POSITION);
+	}
+
+	public static boolean isPickupInShootPosition() {
+		return isPickupInPosition(SHOOT_POSITION);
+	}
+
+	public static boolean isPickupInTrussPosition() {
+		return isPickupInPosition(TRUSS_POSITION);
+	}
+
+	public static boolean isPickupInCatchPosition() {
+		return isPickupInPosition(CATCH_POSITION);
+	}
+
+	public static double getArmAngleAboveHorizontal() {
+		// apply some function to this to convert to angle
+		return RobotSensors.pickupPotentiometer.get() * 73.015 - 179.257;  // Competition robot
+		// return RobotSensors.pickupPotentiometer.get() * 74.522 - 258.68; //Practice robot
+	}
+
+	public static void initialize() {
+		timer = new Timer();
+		timer.start();
+	}
+
+	public static void update() {
+
+		double now = timer.get();
+		if (now - lastTime < 0.2) {
+			velocity = 0.5 * velocity + 0.5 * (getArmAngleAboveHorizontal() - lastPosition) / (now - lastTime);
+		} else {
+			// been more than 0.2 seconds since last time
+			// so assumed it's stopped
+			velocity = 0;
+		}
+		lastTime = now;
+		lastPosition = getArmAngleAboveHorizontal();
+
+		double mechSpeed = 0.0;
+		double targetAngleDifference = armTargetAngle - getArmAngleAboveHorizontal();
+		double targetSpeed = Math.min(1.0, Math.max(-0.75, targetAngleDifference / 15)) * 0.3;
+		// double targetSpeed = Math.min(1.0,Math.max(-0.75,targetAngleDifference / 15)) * 0.3;
+
+		if (Math.abs(targetAngleDifference) < 3.5 + (armTargetAngle < 0 ? 3 : 0)) {
+			targetSpeed = 0;
+		}
+
+		if (targetAngleDifference > 0) {
+			targetSpeed *= 1.5;
+		}
+
+		if (lastPosition > 80 && armTargetAngle < 80) {
+			targetSpeed *= 1.8;
+		}
+
+		if (armTargetAngle > -10 && lastPosition < 0) {
+			targetSpeed = 1;
+		}
+
+		//double targetSpeed = -Math.max(-30 / 2.5, Math.min(30 / 2.5, armTargetAngle - getArmAngleAboveHorizontal())) * 2.5 / 100.0;
+		//negative because down is positive and up is negative
+
+		double amt = -targetSpeed; // since up is negative
+
+		if (amt < 0 && (!isUpperLimitReached())) {
+			mechSpeed = amt;
+		}
+		if (amt > 0 && (!isLowerLimitReached())) {
+			mechSpeed = amt;
+		}
+
+		RobotActuators.pickupMechMotor.set(mechSpeed);
+	}
+
+	public static double getVelocity() {
+		return velocity;
+	}
+}
