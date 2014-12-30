26c89ef05072aa5a4dbd2e8329658e23849b89cd
CurtisFenner <cfenneremail@gmail.com>
2014-12-29 22:01:47 -0500

    ZeroBall fixed (coupled to One ball because of original coupling)

--- AdamBots FRC 2014 Cleanup/src/autons/StandardZeroBallAuton.java
+++ AdamBots FRC 2014 Cleanup/src/autons/StandardZeroBallAuton.java
@@ -5,26 +5,66 @@
  */
 package autons;
 
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import subsystems.RobotDrive;
 import subsystems.RobotPickup;
 import edu.wpi.first.wpilibj.templates.*;
+import subsystems.RobotShoot;
 
 /**
  *
  * @author Tyler
  */
-public class StandardZeroBallAuton extends AutonZero{
+public class StandardZeroBallAuton {
+
+	public static final double STRAIGHT_DISTANCE = 50; // needs to be found in testing
+	public static final double BACKWARDS_DISTANCE = -50; // needs to be found in testing
+	public static double averageDriveEncoder;
+	public static Timer timer;
+	public static double fallTimer = 2.0;
+	public static double closeTime = 2.0;
+	public static int step;
+	public static final double speed = 0.5;
+
+	// reset all Encoders
+	public static void reset() {
+		RobotSensors.leftDriveEncoder.reset();
+		RobotSensors.rightDriveEncoder.reset();
+		//RobotSensors.shooterWinchEncoder.reset();
+		timer.stop();
+		timer.reset();
+	}
+
+	public static void stepOne() {
+		if (timer.get() == 0) {
+			timer.start();
+			RobotShoot.startShoot();
+			System.out.println("start at 0");
+		}
+
+		RobotDrive.disableSmoothing();
+
+		double forward = -1.0;
+
+		if (averageDriveEncoder <= STRAIGHT_DISTANCE) {
+			RobotDrive.drive(forward, forward);
+		} else {
+			RobotDrive.stopDrive();
+		}
+
+		RobotPickup.moveToShootPosition();
+
+		if (RobotPickup.isPickupInShootPosition() && averageDriveEncoder >= STRAIGHT_DISTANCE) {
+			step = 3;
+		}
+	}
 
 	//// VARIABLES -------------------------------------------------------------
-	public static final double speed = 0.5;
-	public static int step;
-	public static double averageDriveEncoder;
-	public static final double STRAIGHT_DISTANCE = 50;
-	public static final double BACKWARDS_DISTANCE = -50;
-	
 	public static void intialize() {
-		AutonZero.initialize();
-		AutonZero.reset();
+		timer = new Timer();
+		step = 1;
+		reset();
 		averageDriveEncoder = 0.0;
 	}
 
@@ -59,6 +99,9 @@ public class StandardZeroBallAuton extends AutonZero{
 
 	// update method
 	public static void update() {
+		averageDriveEncoder = RobotDrive.getEncoderRightTicks();
+		SmartDashboard.putBoolean("Pickup in shoot", RobotPickup.isPickupInShootPosition());
+		StandardOneBallAuton.update();
 		averageDriveEncoder = (RobotDrive.getEncoderLeftTicks() + RobotDrive.getEncoderRightTicks()) / 2.0;
 		switch (step) {
 			case 1:
